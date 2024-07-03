45d714fa99ef3f79bd1bd9eaf7686357a72e4ad5
Stanislav Malakhov <thehustaan@gmail.com>
2024-07-03 19:58:17 +0300

    Add global intercepors and filters

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { FootballModule } from './football/football.module';
 import databaseConfig from './config/database.config';
+import footballApiHeadersConfig from './config/football-api-headers.config';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionFilter } from './filters/all-exception.filter';
+import { RequestsStatisticsModule } from './request-statistics/requests-statistics.module';
+import { RequestDurationInterceptor } from './interceptors/request-duration.interceptor';
+import { RequestsCounterInterceptor } from './interceptors/request-counter.interceptor';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `${process.cwd()}/env/.${process.env.NODE_ENV}.env`,
       isGlobal: true,
-      load: [databaseConfig],
+      load: [databaseConfig, footballApiHeadersConfig],
     }),
+    DatabaseModule,
+    FootballModule,
+    RequestsStatisticsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: RequestDurationInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: RequestsCounterInterceptor,
+    },
+  ],
 })
 export class AppModule {}
